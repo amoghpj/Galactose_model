78ee59b884de7c2088fdff915c71cd500354b78d
Hua, Bo <bflyingbox@gmail.com>
2016-02-08 13:16:31 -0500

    Speed up 100x by using ODE15s instead of ODE45

--- Venturelli2015_Model1/solve_plot_dynamics.m
+++ Venturelli2015_Model1/solve_plot_dynamics.m
@@ -11,10 +11,14 @@ close all
 % set parameters
 setup_params_Venturelli2015_1 % initializes params_model1_qss
 
+% set up the function handle to solve ODE
+% ode_handle = @(varargin) ode45(varargin{:});
+ode_handle = @(varargin) ode15s(varargin{:});  % for stiff problems
+
 % solve ODE
 trange = [0 40];
 x0 = [0 0 0 0]; % G1, R, G80, G4
-[t,x] = ode45(@(t,x) EqnsVenturelli2015_1qss(t,x,params_model1_qss), trange, x0);
+[t,x] = ode_handle(@(t,x) EqnsVenturelli2015_1qss(t,x,params_model1_qss), trange, x0);
 
 % plot time-traces
 figure
